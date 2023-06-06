b956c7a0a0d7b89d110b2e0b577f2f66baabcf70
Taeung <park0579@algonquinlive.com>
2023-06-06 18:55:59 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import { useState, useEffect } from "react";
 
+function Hello(){
+  // useEffect(function(){
+  //   console.log("Hi :)");
+  //   return function (){
+  //     console.log("Bye :(")
+  //   } 
+  // }, []); //same function with below but little longer:
+
+  // useEffect(()=>{
+  //   console.log("Hi :)");
+  //   return () => console.log("Bye :(")
+  // }, []); //same function with below:
+  
+  function byeFn(){
+    console.log("Bye :(")
+  }
+  function hiFn(){
+    console.log("Hi :)");
+    return byeFn;
+  }
+  useEffect(hiFn, []);
+  return <h1>hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setValue((prev) => prev+1);
-  const onChange =(event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("I run only once")
-  }, []);
-  useEffect(()=>{
-    console.log("I run when 'keyowrd' changes");
-  }, [keyword]);
-  useEffect(()=>{
-    console.log("I run when 'counter' changes");
-}, [counter]);
-
-useEffect(()=>{
-  console.log("I run when 'keyowrd' & 'counter' changes");
-}, [keyword, counter]);
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input value={keyword} 
-        onChange={onChange} 
-        type="text" 
-        placeholder="Search here...." 
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me!</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
